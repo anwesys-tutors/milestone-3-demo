151e7ade3d99d641d2623d8d9cf799ebff6b2f1d
Jannis Blossey <jannis@blossey.eu>
2021-07-05 21:20:54 +0200

    create integrationtest for germanrestcontroller

--- src/main/java/com/anwesys/milestone3demo/GermanRepository.java
+++ src/main/java/com/anwesys/milestone3demo/GermanRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.io.Serializable;
+
 @RepositoryRestResource(collectionResourceRel = "germanDictionary", path = "dict")
-public interface GermanRepository extends CrudRepository<German, Long> {
+public interface GermanRepository extends CrudRepository<German, Serializable> {
 
     German findGermanByEnglish(English word);
 
