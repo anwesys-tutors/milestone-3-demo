734db92db6a3918bcbd322cc7390da897c940bf1
Jannis Blossey <jannis@blossey.eu>
2021-07-05 22:09:30 +0200

    fix: Key type is serializable, not long

--- src/main/java/com/anwesys/milestone3demo/EnglishRepository.java
+++ src/main/java/com/anwesys/milestone3demo/EnglishRepository.java
@@ -3,6 +3,8 @@ package com.anwesys.milestone3demo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.io.Serializable;
+
 @RepositoryRestResource(collectionResourceRel = "englishDictionary", path = "en")
-public interface EnglishRepository extends CrudRepository<English, Long> {
+public interface EnglishRepository extends CrudRepository<English, Serializable> {
 }
